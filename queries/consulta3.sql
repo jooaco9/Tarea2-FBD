3dfa890592151fbe23f825fd1b187efe33e659ac
pachecus <126217007+pachecus@users.noreply.github.com>
2024-09-08 22:24:32 -0300

    consulta 3

--- queries/consulta3.sql
+++ queries/consulta3.sql
@@ -2,8 +2,14 @@ SELECT
   t.name, 
   t.milliseconds / 60000 AS duration, 
   t.unit_price
-FROM 
-  track t INNER JOIN invoice_line USING(track_id) INNER JOIN invoice i USING(invoice_id)
-WHERE 
-  milliseconds = (SELECT MAX(milliseconds) FROM track)
-  AND i.billing_country = 'Canadá';
+  FROM track t
+  	INNER JOIN invoice_line il USING(track_id)
+	INNER JOIN invoice i USING(invoice_id)
+WHERE
+	t.milliseconds = (
+		SELECT MAX(milliseconds) 
+		FROM track
+		INNER JOIN invoice_line il2 USING(track_id)
+		INNER JOIN invoice i2 USING(invoice_id)
+		WHERE i2.billing_country = 'Canada'
+	);
