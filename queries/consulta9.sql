3b30cb5cc115158a2d5b7f6362fbf54409fdc053
jooaco9 <joaquin.corbo@gmail.com>
2024-09-09 22:24:55 -0300

    Consulta 9 ya hecha, capaz se puede escribir mejor

---
+++ queries/consulta9.sql
@@ -0,0 +1,40 @@
+-- Devolver el identificador, nombre y apellido del empleado que asesoro la mayor cantidad de ventas en el 2024
+-- Esquema del resultado: employee_id, first_name, last_name.
+
+SELECT
+  e.employee_id,
+  e.first_name,
+  e.last_name
+FROM 
+  employee e
+WHERE
+  (SELECT 
+      COUNT(*) --cantVentasEmp1
+    FROM 
+      employee e1
+      JOIN customer c1 ON employee_id = support_rep_id
+      INNER JOIN invoice i1 USING(customer_id)
+    WHERE 
+      i1.invoice_date::text LIKE '%2024%' AND
+      e1.employee_id = e.employee_id
+    GROUP BY
+      e1.employee_id
+  ) >= ALL (SELECT 
+      COUNT(*) --cantVentasEmpleados
+    FROM 
+      employee e1
+      JOIN customer c1 ON employee_id = support_rep_id
+      INNER JOIN invoice i1 USING(customer_id)
+    WHERE 
+      i1.invoice_date::text LIKE '%2024%'
+    GROUP BY
+      e1.employee_id
+  );
+
+
+
+ 
+
+
+
+
