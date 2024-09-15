5bb9b38d3de799c646578e835544929b1b5bb80b
Nicolas Lettiere <77543125+nicolaslettiere@users.noreply.github.com>
2024-09-15 20:37:15 -0300

    Update consulta7.sql

--- queries/consulta7.sql
+++ queries/consulta7.sql
@@ -1,6 +1,7 @@
 -- Devolver el nombre del g´enero musical con m´as cantidad de playlists.
 -- Esquema del resultado: name. 
 
+-- VERSION 1
 SELECT g.name AS genre_name
 FROM genre g
 JOIN track t ON g.genre_id = t.genre_id
@@ -16,3 +17,17 @@ HAVING COUNT(DISTINCT pt.playlist_id) = (
         ) AS subquery
     )
 ORDER BY g.name;
+
+-- VERSION 2
+SELECT genre_name
+FROM (
+  SELECT g.name AS genre_name,
+  COUNT(DISTINCT pt.playlist_id) AS playlist_count,
+  MAX(COUNT(DISTINCT pt.playlist_id)) OVER () AS max_playlist_count 
+  FROM genre g
+  JOIN track t ON g.genre_id = t.genre_id
+  JOIN playlist_track pt ON t.track_id = pt.track_id
+  GROUP BY g.genre_id, g.name
+)
+WHERE playlist_count = max_playlist_count
+ORDER BY genre_name;
