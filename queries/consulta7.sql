4bc1fd75c8a8bc4aba82aaef6bdbb824e1da20fc
Nicolas Lettiere <77543125+nicolaslettiere@users.noreply.github.com>
2024-09-15 20:26:32 -0300

    Update consulta7.sql fix limit

--- queries/consulta7.sql
+++ queries/consulta7.sql
@@ -7,7 +7,5 @@ JOIN track t ON g.genre_id = t.genre_id
 JOIN playlist_track pt ON t.track_id = pt.track_id
 JOIN playlist p ON pt.playlist_id = p.playlist_id
 GROUP BY g.name
-ORDER BY COUNT(DISTINCT p.playlist_id) DESC
-LIMIT 1;
-
+ORDER BY COUNT(DISTINCT p.playlist_id) DESC;
 
