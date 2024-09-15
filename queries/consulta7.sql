7cfea62666c3235b055c6f9fc2fddbba8814814f
Nicolas Lettiere <77543125+nicolaslettiere@users.noreply.github.com>
2024-09-15 20:33:37 -0300

    Update consulta7.sql

--- queries/consulta7.sql
+++ queries/consulta7.sql
@@ -5,7 +5,14 @@ SELECT g.name AS genre_name
 FROM genre g
 JOIN track t ON g.genre_id = t.genre_id
 JOIN playlist_track pt ON t.track_id = pt.track_id
-JOIN playlist p ON pt.playlist_id = p.playlist_id
-GROUP BY g.name
-ORDER BY COUNT(DISTINCT p.playlist_id) DESC;
-
+GROUP BY g.genre_id, g.name
+HAVING COUNT(DISTINCT pt.playlist_id) = (
+  SELECT MAX(playlist_count) FROM (
+      SELECT COUNT(DISTINCT pt.playlist_id) AS playlist_count FROM 
+        genre g
+        JOIN track t ON g.genre_id = t.genre_id
+        JOIN playlist_track pt ON t.track_id = pt.track_id
+        GROUP BY g.genre_id
+        ) AS subquery
+    )
+ORDER BY g.name;
