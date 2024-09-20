13f2084927594e2ca619849d40e3d0e3ff61d8cb
Nicolas Lettiere <77543125+nicolaslettiere@users.noreply.github.com>
2024-09-20 18:38:09 -0300

    Update consulta10.sql

--- queries/consulta10.sql
+++ queries/consulta10.sql
@@ -6,8 +6,23 @@
  GROUP BY t.track_id
  ORDER BY count DESC;
 
+Selecciona todas las columnas (*) de las tablas involucradas y cuenta el número de veces que 
+cada pista (track) aparece en las líneas de factura (invoice_line) para el año 2013.
+
+Se une la tabla invoice_line (il) con la tabla invoice (i) usando invoice_id. Esto vincula cada línea de factura con su correspondiente factura.
+Se une la tabla invoice_line con la tabla track (t) usando track_id, lo que permite acceder a los detalles de cada pista en las líneas de factura.
+
+se usa WHERE para filtrar las facturas e incluir solo aquellas con una fecha de factura (invoice_date)
+entre el 1 de enero de 2013 y el 31 de diciembre de 2013.
+
+se usa GROUP BY t.track_id para agrupar los resultados por el identificador de la pista. 
+
+los resultados se ordenan en orden descendente (DESC) según el conteo de pistas.
+ 
+RESUMEN:
 La consulta busca identificar y contar cuántas veces se ha facturado cada pista de música en el año 2013.
 Luego, muestra estas pistas ordenadas de la más facturada a la menos facturada.
+
 
 Dato extra: 
 Hay un problema en la consulta, usar SELECT * junto con GROUP BY 
