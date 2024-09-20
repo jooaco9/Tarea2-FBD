585a32b0b8eb24f0b2c957a4e2fa54ae45903808
Nicolas Lettiere <77543125+nicolaslettiere@users.noreply.github.com>
2024-09-20 18:31:37 -0300

    Update consulta7.sql

--- queries/consulta7.sql
+++ queries/consulta7.sql
@@ -1,33 +1,18 @@
 -- Devolver el nombre del g´enero musical con m´as cantidad de playlists.
 -- Esquema del resultado: name. 
 
--- VERSION 1
 SELECT g.name AS genre_name
 FROM genre g
 JOIN track t ON g.genre_id = t.genre_id
 JOIN playlist_track pt ON t.track_id = pt.track_id
-GROUP BY g.genre_id, g.name
+GROUP BY g.name
 HAVING COUNT(DISTINCT pt.playlist_id) = (
-  SELECT MAX(playlist_count) FROM (
-      SELECT COUNT(DISTINCT pt.playlist_id) AS playlist_count FROM 
-        genre g
-        JOIN track t ON g.genre_id = t.genre_id
-        JOIN playlist_track pt ON t.track_id = pt.track_id
-        GROUP BY g.genre_id
-        ) AS subquery
-    )
-ORDER BY g.name;
-
--- VERSION 2
-SELECT genre_name
-FROM (
-  SELECT g.name AS genre_name,
-  COUNT(DISTINCT pt.playlist_id) AS playlist_count,
-  MAX(COUNT(DISTINCT pt.playlist_id)) OVER () AS max_playlist_count 
-  FROM genre g
-  JOIN track t ON g.genre_id = t.genre_id
-  JOIN playlist_track pt ON t.track_id = pt.track_id
-  GROUP BY g.genre_id, g.name
-)
-WHERE playlist_count = max_playlist_count
-ORDER BY genre_name;
+        SELECT MAX(playlist_count)
+        FROM (
+            SELECT COUNT(DISTINCT pt.playlist_id) AS playlist_count
+            FROM genre g2
+            JOIN track t2 ON g2.genre_id = t2.genre_id
+            JOIN playlist_track pt ON t2.track_id = pt.track_id
+            GROUP BY g2.name
+        )
+  );
