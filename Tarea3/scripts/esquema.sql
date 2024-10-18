c079481870afa30e58eb7fe7c9b1a0d2c1f8334d
Nicolas Lettiere <nicolaslettiereq@gmail.com>
2024-10-18 17:18:24 -0300

    .

---
+++ Tarea3/scripts/esquema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE public.paises_lideres (
+    id_pais character varying,
+    n_pais character varying,
+    d_pais character varying,
+    id_capital character varying,
+    n_capital character varying,
+    d_capital character varying,
+    cargo character varying,
+    id_lider character varying,
+    n_lider character varying,
+    bio_lider character varying,
+    id_ciudad character varying,
+    n_ciudad character varying,
+    d_ciudad character varying,
+    id_pers character varying,
+    n_pers character varying,
+    fecha_nac character varying,
+    fecha_fallec character varying,
+    id_lugarfallec character varying,
+    tipo_lugarfallec character varying,
+    n_lugarfallec character varying
+);
+
