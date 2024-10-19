426eb3ed01b1c4183354c1a2a4cd4a034df06bc9
Nicolas Lettiere <nicolaslettiereq@gmail.com>
2024-10-19 19:52:21 -0300

    avance hasta 1.d

--- Tarea3/queries.sql
+++ Tarea3/queries.sql
@@ -51,7 +51,7 @@ WHERE
 
 -- 1. b. ii) Aplicando 1. a. i)
 SELECT
-    DISTINCT pl1.id_pers,
+    pl1.id_pers,
     pl1.tipo_lugarfallec
 FROM
     paises_lideres as pl1
@@ -125,3 +125,76 @@ HAVING
         OR COUNT(DISTINCT fecha_nac) > 1
         OR COUNT(DISTINCT id_lugarfallec) > 1
     );
+    
+/*
+	1.c) La consulta (i) se mantuvo sin resultados
+		 La consulta (ii) y la consulta (iii) dan diferentes resultados en la consulta que tiene group by de la que no
+		 En la que tiene group by devuelve resultados nulos, supongo que tiene que ver con la naturaleza del group by
+		 ya que en las consultas sin el group by se hacen las comparaciones especificamente basadas en el atributo
+*/;
+
+-- 1. d. i) Sin group by
+SELECT
+    X, Y
+FROM
+    R as r1
+WHERE
+    EXISTS (
+    	SELECT 
+    		Y 
+    	FROM 
+    		R as r2
+        WHERE
+            r1.X = r2.X
+            AND r1.Y <> r2.Y
+    );
+
+SELECT
+	pl1.id_pers, pl1.tipo_lugarfallec, pl1.id_lugarfallec
+FROM
+	paises_lideres as pl1
+WHERE
+	EXISTS (
+		SELECT
+			pl2.id_lugarfallec
+		FROM
+			paises_lideres as pl2
+		WHERE
+			pl1.id_pers = pl2.id_pers
+			AND pl1.tipo_lugarfallec = pl2.tipo_lugarfallec
+			AND pl1.id_lugarfallec <> pl2.id_lugarfallec
+	);
+
+-- 1. d. ii) Con group by
+SELECT
+	r1.X, r1.Y
+FROM
+	R as r1
+JOIN (
+	SELECT
+		r2.X
+	FROM
+		R as r2
+	GROUP BY
+		X
+	HAVING COUNT(DISTINCT Y) > 1
+) as R3
+ON r1.X = r2.X
+;
+
+SELECT
+	pl.id_pers, pl.tipo_lugarfallec, pl.id_lugarfallec
+FROM
+	paises_lideres as pl
+JOIN (
+	SELECT
+		pl2.id_pers, pl2.tipo_lugarfallec
+	FROM
+		paises_lideres as pl2
+	GROUP BY
+		pl2.id_pers, pl2.tipo_lugarfallec
+	HAVING COUNT(DISTINCT pl2.id_lugarfallec) > 1
+) as sq
+ON pl.id_pers = sq.id_pers
+AND pl.tipo_lugarfallec = sq.tipo_lugarfallec
+;
